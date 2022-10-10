5df954f861f2e6189cdec920ed40004219ac755c
Воронин Вячеслав <114893510+VoroninVaycheslav@users.noreply.github.com>
2022-10-10 14:06:45 +0700

    Add files via upload

---
+++ Программы на пайтоне/Python_CPlusPlus.py
@@ -0,0 +1,18 @@
+a= int(input())         # int a = 0;
+                        # cin >> a;      
+if a > 10 | a<15:              # if(a>10 || a <15){}
+   print(a)             # cout << a;
+for i in range(0,a):    # for(int i =0; i < a; i++){}
+    print(a)
+
+def asd(x):             # void asd(int x){}
+    print(x)
+
+b = int(input()) 
+c = a-b                 # int c = a-b;
+d = a+b                 # int d = a+b;
+f = a/b                 # int f = a/b;
+g = a%b                 # int g = a%b;
+#map<string,int> mp; - массив , который хранит string и int.
+#vector<int> ads - вектор
+#int[] - массив.
